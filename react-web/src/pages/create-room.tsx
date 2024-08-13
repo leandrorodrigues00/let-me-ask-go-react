d20fdbbf45b0fc7005be2fa01eb2f130015ffd6b
leandro <leandro.rodrigues00@hotmail.com>
2024-08-12 21:55:47 -0300

    feature/3 - createRoom page structure

--- react-web/src/pages/create-room.tsx
+++ react-web/src/pages/create-room.tsx
@@ -1,3 +1,35 @@
+import { ArrowRight } from "lucide-react";
+import amaLogo from "../assets/ama-logo.svg";
+
 export function CreateRoom() {
-  return <h1>Create Room</h1>;
+  return (
+    <main className="h-screen flex items-center justify-center px-4">
+      <div className="max-w-[450px] flex flex-col gap-6">
+        <img src={amaLogo} alt="AMA" className="h-10" />
+
+        <p className="leading-relaxed text-zinc-300 text-center">
+          Create a public AMA (Ask me anything) room and prioritize the most
+          important questions for the community.
+        </p>
+
+        <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
+          <input
+            type="text"
+            name="theme"
+            placeholder="Room name"
+            className="flex-1 text-sm bg-transparent mx-2 outline-none placeholder:text-zinc-500 text-zinc-100"
+            autoComplete="off"
+          />
+
+          <button
+            type="submit"
+            className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
+          >
+            Create room
+            <ArrowRight className="size-4" />
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 }
