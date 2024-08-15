a0b531e5cc365f1bc192a0172414cc468f7f95ad
leandro <leandro.rodrigues00@hotmail.com>
2024-08-14 22:22:53 -0300

    feature/5 - add room page

--- react-web/src/pages/room.tsx
+++ react-web/src/pages/room.tsx
@@ -1,7 +1,67 @@
 import { useParams } from "react-router-dom";
+import { ArrowRight, ArrowUp, Share2 } from "lucide-react";
+import { toast } from "sonner";
+
+import amaLogo from "../assets/ama-logo.svg";
+import { Message } from "../components/message";
 
 export function Room() {
   const { roomId } = useParams();
 
-  return <div>{roomId}</div>;
+  function handleShareRoom() {
+    const url = window.location.href.toString();
+
+    if (navigator.share !== undefined && navigator.canShare()) {
+      navigator.share({ url });
+    } else {
+      navigator.clipboard.writeText(url);
+
+      toast.info("The room link has been copied to the clipboard!");
+    }
+  }
+
+  return (
+    <div className="mx-auto max-w-[640px] flex flex-col gap-6 py-10 px-4">
+      <div className="flex items-center gap-3 px-3">
+        <img src={amaLogo} alt="AMA" className="h-5" />
+
+        <span className="text-sm text-zinc-500 truncate">
+          Room code: <span className="text-zinc-300">{roomId}</span>
+        </span>
+
+        <button
+          type="submit"
+          className="ml-auto bg-zinc-800 text-zinc-300 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-zinc-700"
+          onClick={handleShareRoom}
+        >
+          Share
+          <Share2 className="size-4" />
+        </button>
+      </div>
+
+      <div className="h-px w-full bg-zinc-900" />
+
+      <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
+        <input
+          type="text"
+          name="theme"
+          placeholder="What's your question?"
+          className="flex-1 text-sm bg-transparent mx-2 outline-none placeholder:text-zinc-500 text-zinc-100"
+          autoComplete="off"
+        />
+
+        <button
+          type="submit"
+          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
+        >
+          Create question
+          <ArrowRight className="size-4" />
+        </button>
+      </form>
+      <ol className="list-decimal list-outside px-3 space-y-8">
+        <Message text="text message 1 " amountOfReactions={15} answered />
+        <Message text="text message 2" amountOfReactions={10} />
+      </ol>
+    </div>
+  );
 }
