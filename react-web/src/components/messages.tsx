d7a21dd001e01f706ff70fac76a44cbdc6955298
leandro <leandro.rodrigues00@hotmail.com>
2024-08-18 13:43:30 -0300

    feature/10 - add listing of new messages with websocket

--- react-web/src/components/messages.tsx
+++ react-web/src/components/messages.tsx
@@ -1,10 +1,15 @@
 import { useParams } from "react-router-dom";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 
 import { Message } from "./message";
-import { getRoomMessages } from "../http/get-room-messages";
+import {
+  getRoomMessages,
+  type GetRoomMessagesResponse,
+} from "../http/get-room-messages";
+import { useEffect } from "react";
 
 export function Messages() {
+  const queryClient = useQueryClient();
   const { roomId } = useParams();
 
   if (!roomId) {
@@ -15,6 +20,51 @@ export function Messages() {
     queryKey: ["messages", roomId],
     queryFn: () => getRoomMessages({ roomId }),
   });
+
+  useEffect(() => {
+    const ws = new WebSocket(`ws://localhost:8080/subscribe/${roomId}`);
+
+    ws.onopen = () => {
+      console.log("websocket connected!");
+    };
+
+    ws.onclose = () => {
+      console.log("websocket connection closed!");
+    };
+
+    ws.onmessage = (event) => {
+      const data: {
+        kind: "message_created";
+        value: any;
+      } = JSON.parse(event.data);
+
+      switch (data.kind) {
+        case "message_created":
+          queryClient.setQueryData<GetRoomMessagesResponse>(
+            ["messages", roomId],
+            (state) => {
+              return {
+                messages: [
+                  ...(state?.messages ?? []),
+                  {
+                    id: data.value.id,
+                    text: data.value.message,
+                    amountOfReactions: 0,
+                    answered: false,
+                  },
+                ],
+              };
+            }
+          );
+
+          break;
+      }
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, [roomId, queryClient]);
 
   return (
     <ol className="list-decimal list-outside px-3 space-y-8">
