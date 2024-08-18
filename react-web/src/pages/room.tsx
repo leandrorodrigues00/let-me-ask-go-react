375083d349c707add9441c827cdc94ee1b554b23
leandro <leandro.rodrigues00@hotmail.com>
2024-08-18 12:40:18 -0300

    feature/8 - add form to create messages

--- react-web/src/pages/room.tsx
+++ react-web/src/pages/room.tsx
@@ -1,10 +1,11 @@
+import { Suspense } from "react";
 import { useParams } from "react-router-dom";
-import { ArrowRight, Share2 } from "lucide-react";
+import { Share2 } from "lucide-react";
 import { toast } from "sonner";
 
 import amaLogo from "../assets/ama-logo.svg";
 import { Messages } from "../components/messages";
-import { Suspense } from "react";
+import { CreateMessageForm } from "../components/create-message-form";
 
 export function Room() {
   const { roomId } = useParams();
@@ -42,23 +43,7 @@ export function Room() {
 
       <div className="h-px w-full bg-zinc-900" />
 
-      <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
-        <input
-          type="text"
-          name="theme"
-          placeholder="What's your question?"
-          className="flex-1 text-sm bg-transparent mx-2 outline-none placeholder:text-zinc-500 text-zinc-100"
-          autoComplete="off"
-        />
-
-        <button
-          type="submit"
-          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
-        >
-          Create question
-          <ArrowRight className="size-4" />
-        </button>
-      </form>
+      <CreateMessageForm />
 
       <Suspense fallback={<p>Carregando...</p>}>
         <Messages />
